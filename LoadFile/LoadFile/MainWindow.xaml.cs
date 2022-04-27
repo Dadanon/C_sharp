56afc6e2cb57d6bfc8d2c6eaf56808868f85ad79
Dadanon <mar4ello19922@gmail.com>
2022-04-27 11:51:01 +0300

    Final STL Reader

---
+++ LoadFile/LoadFile/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Win32;
+
+namespace LoadFile
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void ImageLoad(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.InitialDirectory = "C:\\";
+
+            if (ofd.ShowDialog() == true)
+            {
+                FileImage.Text = "YEAH!";
+            }
+        }
+    }
+}
